10d2c366c0b80d3214c595618613ebe65542473c
saisreereddy <v.saisree.reddy@gmail.com>
2024-02-06 13:48:09 +0530

    Update main.bundle.js

--- main.bundle.js
+++ main.bundle.js
@@ -558,7 +558,7 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var RegionService = /** @class */ (function () {
     function RegionService(_http) {
         this._http = _http;
-        this.baseUrl = 'https://restcountries.com/v2/all';
+        this.baseUrl = 'https://restcountries.com/v2';
         console.log("RegionService is called");
     }
     RegionService.prototype.handleError = function (err) {
